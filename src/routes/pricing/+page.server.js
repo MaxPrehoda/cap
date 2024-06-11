3f682f64dc98a36b92af2bf827f0fbe46d72e967
MaxPrehoda <maxprehoda@gmail.com>
2024-06-11 13:32:31 -0700

    lesson push

---
+++ src/routes/pricing/+page.server.js
@@ -0,0 +1,28 @@
+import { Stripe } from "stripe";
+import { redirect } from "@sveltejs/kit";
+
+const stripe = new Stripe("sk_test");
+
+export async function load() {}
+
+/*
+checkout: async ({ locals, request }) => {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1OhbZ4AgGZYlcp7ia59OLLut",
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      success_url: "http://localhost:5173/success/{CHECKOUT_SESSION_ID}",
+      cancel_url: "http://localhost:5173/",
+    });
+
+    if (session.url) {
+      redirect(303, session.url);
+    } else {
+      throw new Error("Stripe Checkout session URL is missing");
+    }
+  },*/
--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -2,7 +2,6 @@
   import { Card } from "flowbite-svelte";
   import { CheckCircleSolid } from "flowbite-svelte-icons";
   import { Button } from "flowbite-svelte";
- 
 </script>
 
 <div class="h-full w-full relative">
@@ -33,26 +32,40 @@
   </div>
   <div class="flex flex-col lg:flex-row gap-8 mx-auto">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mx-auto py-14">
-      <Card class="h-fit relative border-amber-600  border-2 "  padding="xl" >
-        <div class= " bg-gradient-to-r from-red-500 to-orange-500 absolute flex items-center gap-1 -top-0 -translate-y-1/2 left-8 studio-primary-gradient rounded py-1 px-2.5 text-xxs tracking-widest text-white"> 
+      <Card class="h-fit relative border-amber-600  border-2 " padding="xl">
+        <div
+          class=" bg-gradient-to-r from-red-500 to-orange-500 absolute flex items-center gap-1 -top-0 -translate-y-1/2 left-8 studio-primary-gradient rounded py-1 px-2.5 text-xxs tracking-widest text-white"
+        >
           <span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+              />
             </svg>
           </span>
-          
-          Popular</div>
-        
-        <h5 class="mb-4 text-xl font-medium  text-gray-500 dark:text-gray-400">
+
+          Popular
+        </div>
+
+        <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
           Pro plan
         </h5>
         <div
-          class="flex items-baseline  text-gray-900 dark:text-white justify-content: center;"
+          class="flex items-baseline text-gray-900 dark:text-white justify-content: center;"
         >
           <span class="text-3xl font-semibold">$</span>
           <span class="text-5xl font-extrabold tracking-tight">15</span>
           <span
-            class="ms-1 text-xl font-normal  text-gray-500 dark:text-gray-400"
+            class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400"
             >/month</span
           >
         </div>
@@ -97,35 +110,41 @@
             >
               Access to future updates and new features
             </span>
-            <li class="flex space-x-2 rtl:space-x-reverse">
-              <CheckCircleSolid
-                class="w-4 h-4 text-primary-600 dark:text-primary-500"
-              />
-              <span
-                class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400"
-              >
-                Priority support 
-              </span>
-              <li class="flex space-x-2 rtl:space-x-reverse">
-                <CheckCircleSolid
-                  class="w-4 h-4 text-primary-600 dark:text-primary-500"
-                />
-                <span
-                  class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400"
-                >
-                Advanced templates
-                </span>
-                <li class="flex space-x-2 rtl:space-x-reverse">
-                  <CheckCircleSolid
-                    class="w-4 h-4 text-primary-600 dark:text-primary-500"
-                  />
-                  <span
-                    class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400"
-                  >
-                  Batch processing
-                  </span>
+          </li>
+          <li class="flex space-x-2 rtl:space-x-reverse">
+            <CheckCircleSolid
+              class="w-4 h-4 text-primary-600 dark:text-primary-500"
+            />
+            <span
+              class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400"
+            >
+              Priority support
+            </span>
+          </li>
+          <li class="flex space-x-2 rtl:space-x-reverse">
+            <CheckCircleSolid
+              class="w-4 h-4 text-primary-600 dark:text-primary-500"
+            />
+            <span
+              class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400"
+            >
+              Advanced templates
+            </span>
+          </li>
+          <li class="flex space-x-2 rtl:space-x-reverse">
+            <CheckCircleSolid
+              class="w-4 h-4 text-primary-600 dark:text-primary-500"
+            />
+            <span
+              class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400"
+            >
+              Batch processing
+            </span>
+          </li>
         </ul>
-        <Button href="/">Get Started</Button>
+        <form action="?/subscribepro" method="POST">
+          <Button class="w-full" type="submit">Get Started</Button>
+        </form>
       </Card>
 
       <Card class="h-fit" padding="xl">
@@ -220,5 +239,3 @@
     </div>
   </div>
 </div>
-
-
